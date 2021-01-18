ba31d47e6f2aadcecf5ac09398ab5587207af16b
pontaya411 <ayatomita1@gmail.com>
2021-01-18 21:49:11 +0900

    バリデーション設定〜Rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :last_name, presence:true
-         validates :first_name, presence:true
+      with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do 
+         validates :last_name
+         validates :first_name
+      end
+      with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width katakana characters' } do 
          validates :last_name_kana, presence:true
          validates :first_name_kana, presence:true
+      end  
+      
          validates :birthday, presence:true
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
          
 end
