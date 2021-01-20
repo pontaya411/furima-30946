8574c3e31d9cc1b94231eb3c3cc41f5ef993d840
pontaya411 <ayatomita1@gmail.com>
2021-01-20 22:39:45 +0900

    email、passwordのテストコード記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,25 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors.full_messages).to include("Password can't be blank")
   end 
+  it "パスワードは、6文字以上での入力が必須であること" do
+    user = User.new(nickname: "aya", email: "test@example", password: "00000", password_confirmation: "000000")
+    user.valid?
+    expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+  end
+  it "メールアドレスは、@を含む必要があること" do
+    user = User.new(nickname: "aya", email: "test@example", password: "00000", password_confirmation: "000000")
+    user.valid?
+    expect(user.errors.full_messages).to include("メールアドレスに「＠」を挿入してください")
+  end
+  it "パスワードとパスワード（確認用）、値の一致が必須であること" do
+  user = User.new(nickname: "aya", email: "test@example", password: "000000", password_confirmation: "100000")
+    user.valid?
+    expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+  end
+  it "重複したemailが存在する場合登録できないこと" do
+    user = User.new(nickname: "aya", email: "ayatomita1@gmail.com", password: "000000", password_confirmation: "000000")
+    user.valid?
+    expect(user.errors.full_messages).to include("Email has already been taken")
+  end
  end
 end
