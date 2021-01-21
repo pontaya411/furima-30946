0f3e68f1d932fc41503cefa4405d11b058245615
pontaya411 <ayatomita1@gmail.com>
2021-01-21 21:33:29 +0900

    rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"test"}
-    email                 {"test@example"}
-    password              {"a00000"}
-    password_confirmation {password}
-    last_name             {"鈴川"}
-    first_name            {"綾"}
-    last_name_kana        {"スズカワ"}
-    first_name_kana       {"アヤ"}
-    birthday              {19850411}
+    nickname              { 'test' }
+    email                 { 'test@example' }
+    password              { 'a00000' }
+    password_confirmation { password }
+    last_name             { '鈴川' }
+    first_name            { '綾' }
+    last_name_kana        { 'スズカワ' }
+    first_name_kana       { 'アヤ' }
+    birthday              { 19_850_411 }
   end
 end
