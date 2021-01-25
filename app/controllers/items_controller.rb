2e45d2bd5bdccc4947c788338a1b6c0ddc71423a
pontaya411 <ayatomita1@gmail.com>
2021-01-25 21:42:12 +0900

    ActiveStorage導入２
    コントローラーとモデルに画像を保存できるように記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  # imageという名前で送られできた画像ファイルの保存を許可する記述
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :buy
-  
+  # 下記、ActiveStorageを導入し、itemテーブルのレコードと画像を紐づけるためのメソッド記述
+  has_one_attached :image
   
 end
