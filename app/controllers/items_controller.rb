ff050eb80caab4d53725169049c2464aa5b45020
pontaya411 <ayatomita1@gmail.com>
2021-01-27 22:46:51 +0900

    新規投稿画面への遷移のための記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,20 @@
 class ItemsController < ApplicationController
+  # ログインしていないユーザーをログイン画面に促す記述
+  before_action :authenticate_user!, only: [:new, :create]
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,21 @@ class Item < ApplicationRecord
   has_one :buy
   # 下記、ActiveStorageを導入し、itemテーブルのレコードと画像を紐づけるためのメソッド記述
   has_one_attached :image
-  
+
+  # ActiveHashに下記のカラムが紐づいている
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  belongs_to :category
+  belongs_to :ship_fee
+  belongs_to :ship_date
+  belongs_to :prefecture
+
+  # with_optionsでバリデーションをまとめる。ジャンルの選択が「--」(id:1)の時は保存できないようにするバリデーション
+  with_options numericality: { other_than: 1 } do
+    validates :status_id
+    validates :category_id
+    validates :ship_fee_id
+    validates :ship_date_id
+    validates :prefecture_id
+  end
 end
