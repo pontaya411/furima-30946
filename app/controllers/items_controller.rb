5ba1a30169eeac218d23163bf083bee9a75220f3
pontaya411 <ayatomita1@gmail.com>
2021-02-11 15:25:07 +0900

    destroyアクション追加（レビュー後）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   # ログインしていないユーザーをログイン画面に促す記述
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :contributor_confirmation, only: [:edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   def index
     # 出品投稿の新しい順に表示できるようorderメソッド記述
     @items = Item.all.order(created_at: :desc)
