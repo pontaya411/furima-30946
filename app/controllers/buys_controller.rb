d313e012008f89ddff4d918f958fa5e6e1ecc697
pontaya411 <ayatomita1@gmail.com>
2021-02-19 23:14:39 +0900

    購入画面に遷移できるユーザーの制限を定義

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,13 +1,13 @@
 class BuysController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  
+  before_action :authenticate_user!, only: [:index, :create ]
+  before_action :set_item, only: [:index, :create ]
+  before_action :contributor_confirmation, only: [:create]
+
   def index
-    @item = Item.find(params[:item_id])
     @buyaddresses = BuyAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buyaddresses = BuyAddress.new(buy_params)
     if @buyaddresses.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
@@ -26,6 +26,14 @@ class BuysController < ApplicationController
   private
   def buy_params
     params.require(:buy_address).permit(:prefecture_id, :post_code, :city, :address_number, :building_name, :phone_number, :item_id, :user_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+   def set_item
+      @item = Item.find(params[:item_id])
+   end
+
+   def contributor_confirmation
+    redirect_to root_path unless current_user.id == @item.user.id
   end
   
 end
