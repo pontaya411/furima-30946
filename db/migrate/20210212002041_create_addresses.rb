e9a93417aa541cef624eb373e36a0fd535cb427d
pontaya411 <ayatomita1@gmail.com>
2021-02-24 21:23:47 +0900

    JSの記述追加（レビュー後）

--- db/migrate/20210212002041_create_addresses.rb
+++ db/migrate/20210212002041_create_addresses.rb
@@ -2,9 +2,9 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string :post_code,         null: false
-      t.integer :prefecture_id,                null: false
+      t.integer :prefecture_id,    null: false
       t.string :city,              null: false
-      t.string :phone_number,     null: false
+      t.string :phone_number,      null: false
       t.string :building_name  
       t.string :address_number, null: false
       t.references :buy,                     null: false,  foreign_key: true
